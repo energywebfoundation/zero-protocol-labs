d506b1c8c8ed1202bbb143aa99015da637df36aa
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2021-08-24 19:33:37 +0200

    chore(backend): global config module + env variables defaults and validations

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -2,9 +2,27 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+      isGlobal: true,
+      validationOptions: {
+        allowUnknown: true,
+        abortEarly: false
+      },
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test', 'provision')
+          .default('development'),
+
+        PORT: Joi.number().default(3333)
+      })
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
