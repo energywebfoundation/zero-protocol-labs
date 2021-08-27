e1d0b9125ccf85c88b4eac3bedebe79dd7b8abb3
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2021-08-27 18:01:01 +0200

    chore(backend): mocked /api/partners/filecoin/purchases/{id} response

--- apps/backend/src/purchases/purchases.controller.ts
+++ apps/backend/src/purchases/purchases.controller.ts
@@ -40,7 +40,43 @@ export class PurchasesController {
   @Get(':id')
   @ApiOkResponse({ type: PurchaseDto })
   findOne(@Param('id') id: string) {
-    return this.purchasesService.findOne(+id);
+    return new PurchaseDto({
+      "id": "6ffdd03c-8110-46a9-beb3-c91ddd2e159d",
+      "seller": {
+        "name": "Monsoon Carbon",
+        "address": "Mt Arrakis 42, Dune plains, \nAix en Provence, 12345, France",
+        "contactPerson": "Paul Atreides"
+      },
+      "buyer": {
+        "id": '5ff1cb39-da8b-4f0a-a17d-a5d00ea85a60',
+        "filecoinMinerId": "f0112027",
+        "name": "-"
+      },
+      "files": [
+        {
+          "id": "3e0367d7-0472-4840-bc2d-5e90d84ff5f4",
+          "fileName": "I-REC_RedemptionCertificate_20210802-1234-ABD...EFG.pdf",
+          "url": "https://localhost:3333/api/files/3e0367d7-0472-4840-bc2d-5e90d84ff5f4"
+        }
+      ],
+      recsTransactions: [
+        { year: 2020, amount: 2 },
+        { year: 2021, amount: 1 }
+      ],
+      "certificate": {
+        sellerId: "118007",
+        generatorName: "Solar 1 - Non Bua Lampon",
+        generatorId: "NA",
+        namePlateCapacity: "59.595",
+        namePlateCapacityUnit: "MW",
+        fuelType: "Solar",
+        recsSold: 3,
+        generationStart: new Date("2020-11-01T00:00:00.000Z"),
+        generationEnd: new Date("2021-06-01T23:59:59.999Z")
+      }
+    });
+
+    // return this.purchasesService.findOne(+id);
   }
 
   @Put(':id')
