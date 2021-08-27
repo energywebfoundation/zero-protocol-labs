cbafee07e2c171e5c815a1f8e7baa8af2d56364d
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2021-08-27 17:50:18 +0200

    chore(backend): added annotatations and guards

--- apps/backend/src/purchases/purchases.controller.ts
+++ apps/backend/src/purchases/purchases.controller.ts
@@ -1,18 +1,37 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Body,
+  ClassSerializerInterceptor,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  UseGuards,
+  UseInterceptors
+} from '@nestjs/common';
 import { PurchasesService } from './purchases.service';
 import { CreatePurchaseDto } from './dto/create-purchase.dto';
 import { UpdatePurchaseDto } from './dto/update-purchase.dto';
+import { ApiSecurity, ApiTags } from "@nestjs/swagger";
+import { AuthGuard } from "@nestjs/passport";
 
-@Controller('purchases')
+@Controller('/partners/filecoin/purchases')
+@ApiTags('Filecoin purchases')
+@UseInterceptors(ClassSerializerInterceptor)
 export class PurchasesController {
   constructor(private readonly purchasesService: PurchasesService) {}
 
   @Post()
+  @UseGuards(AuthGuard('api-key'))
+  @ApiSecurity('api-key', ['api-key'])
   create(@Body() createPurchaseDto: CreatePurchaseDto) {
     return this.purchasesService.create(createPurchaseDto);
   }
 
   @Get()
+  @UseGuards(AuthGuard('api-key'))
+  @ApiSecurity('api-key', ['api-key'])
   findAll() {
     return this.purchasesService.findAll();
   }
@@ -22,12 +41,16 @@ export class PurchasesController {
     return this.purchasesService.findOne(+id);
   }
 
-  @Patch(':id')
+  @Put(':id')
+  @UseGuards(AuthGuard('api-key'))
+  @ApiSecurity('api-key', ['api-key'])
   update(@Param('id') id: string, @Body() updatePurchaseDto: UpdatePurchaseDto) {
     return this.purchasesService.update(+id, updatePurchaseDto);
   }
 
   @Delete(':id')
+  @UseGuards(AuthGuard('api-key'))
+  @ApiSecurity('api-key', ['api-key'])
   remove(@Param('id') id: string) {
     return this.purchasesService.remove(+id);
   }
