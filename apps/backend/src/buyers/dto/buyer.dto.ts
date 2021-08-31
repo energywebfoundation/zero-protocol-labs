7bf87acefc5242f3bd897c4d90829a48693d873d
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2021-08-31 17:33:21 +0200

    refactor(backend): renamed folder

--- apps/backend/src/buyers/dto/buyer.dto.ts
+++ apps/backend/src/buyers/dto/buyer.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { Buyer } from "@prisma/client";
-import { FilecoinNodeDto } from "../../filecoinNodes/dto/filecoin-node.dto";
+import { FilecoinNodeDto } from "../../filecoin-nodes/dto/filecoin-node.dto";
 
 export class BuyerDto implements Buyer{
   @ApiProperty({ example: '29e25d61-103a-4710-b03d-ee12df765066' })
