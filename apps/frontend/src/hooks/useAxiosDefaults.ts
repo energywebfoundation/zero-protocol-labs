1a7c42072dc73e8ed2ba95a7ccb79c822555db54
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2021-09-30 11:22:29 +0200

    refactor: renamed API_HOST_URL to API_BASE_URL

--- apps/frontend/src/hooks/useAxiosDefaults.ts
+++ apps/frontend/src/hooks/useAxiosDefaults.ts
@@ -3,11 +3,11 @@ import axios from 'axios';
 declare global {
   interface Window {
     config: {
-      API_HOST_URL: string;
+      API_BASE_URL: string;
     };
   }
 }
 
 export const useAxiosDefaults = () => {
-  axios.defaults.baseURL = window.config.API_HOST_URL;
+  axios.defaults.baseURL = window.config.API_BASE_URL;
 };
