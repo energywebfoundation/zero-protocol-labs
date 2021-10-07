0ebf439348de33cb32f6ca16be046e0fcd6a831b
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2021-10-07 17:22:15 +0200

    chore: added a issuer-api service to the stack

---
+++ apps/issuer-api/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from "@nestjs/config";
+import * as Joi from "joi";
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+      isGlobal: true,
+      validationOptions: {
+        allowUnknown: true,
+        abortEarly: false
+      },
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test')
+          .default('development'),
+        PORT: Joi.number().default(3333),
+        LOG_LEVELS: Joi.string().default('log,error,warn,debug,verbose'),
+      })
+    })
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
