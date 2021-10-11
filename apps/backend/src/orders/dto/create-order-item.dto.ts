71b99633ea9b9e339e73cd04d602756389623fbe
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2021-10-11 16:06:10 +0200

    chore(backend): order item timeframe sequence validation

--- apps/backend/src/orders/dto/create-order-item.dto.ts
+++ apps/backend/src/orders/dto/create-order-item.dto.ts
@@ -1,5 +1,15 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { ArrayMinSize, IsArray, IsEnum, IsNotEmpty, IsString, ValidateNested } from "class-validator";
+import {
+  ArrayMinSize,
+  IsArray,
+  IsEnum,
+  IsNotEmpty,
+  IsString,
+  registerDecorator,
+  ValidateNested,
+  ValidationArguments,
+  ValidationOptions
+} from "class-validator";
 import { Countries } from "@energyweb/utils-general";
 import { CreateOrderItemTimeframeDto } from "./create-order-item-timeframe.dto";
 import { Type } from "class-transformer";
@@ -19,5 +29,32 @@ export class CreateOrderItemDto {
   @ArrayMinSize(1)
   @Type(() => CreateOrderItemTimeframeDto)
   @ValidateNested()
+  @TimeframesInSequence({ message: "timeFrames items start values need to be sorted chronologically" })
   timeFrames: CreateOrderItemTimeframeDto[];
+}
+
+function TimeframesInSequence(validationOptions?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      name: "TimeframesInSequence",
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      validator: {
+        validate(value: any, args: ValidationArguments) {
+          if (value.length < 2) {
+            return true;
+          }
+
+          for (let i = 0; i < value.length - 1; i++) {
+            if (value[i].start >= value[i + 1].start) {
+              return false;
+            }
+          }
+
+          return true;
+        }
+      }
+    });
+  };
 }
