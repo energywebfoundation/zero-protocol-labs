936a6cdc003772052fca347d5a5dac6f6eda3468
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2021-10-11 17:48:21 +0200

    chore(backend): validate if order item timeframes contain full days

--- apps/backend/src/orders/dto/create-order-item-timeframe.dto.ts
+++ apps/backend/src/orders/dto/create-order-item-timeframe.dto.ts
@@ -12,10 +12,12 @@ import {
 export class CreateOrderItemTimeframeDto {
   @ApiProperty({ example: '2020-10-11T00:00:00.000Z' })
   @IsISO8601({ strict: true })
+  @IsStartOfDay({ message: "start should be the first millisecond of a day" })
   start: Date;
 
   @ApiProperty({ example: '2020-12-31T23:59:59.999Z' })
   @IsISO8601({ strict: true })
+  @IsEndOfDay({ message: "end should be the last millisecond of a day" })
   @IsGreaterThan("start", { message: "end cannot be smaller or equal to start" })
   end: Date;
 
@@ -24,6 +26,38 @@ export class CreateOrderItemTimeframeDto {
   @IsInt()
   @Min(1)
   energy: number;
+}
+
+function IsStartOfDay(validationOptions?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      name: "IsStartOfDay",
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      validator: {
+        validate(value: any, validationArguments?: ValidationArguments): boolean {
+          return value.match(/T00:00:00\.000Z$/);
+        }
+      }
+    });
+  };
+}
+
+function IsEndOfDay(validationOptions?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      name: "IsEndOfDay",
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      validator: {
+        validate(value: any, validationArguments?: ValidationArguments): boolean {
+          return value.match(/T23:59:59\.999Z$/);
+        }
+      }
+    });
+  };
 }
 
 function IsGreaterThan(property: string, validationOptions?: ValidationOptions) {
