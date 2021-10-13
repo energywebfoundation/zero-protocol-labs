5a29bb287dc2966b7fd2ce9064a00719959b8f52
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2021-10-13 13:18:25 +0200

    fix(backend): fixed Swagger document title

--- apps/backend/src/swagger/SwaggerDocumentConfig.ts
+++ apps/backend/src/swagger/SwaggerDocumentConfig.ts
@@ -2,7 +2,7 @@ import { DocumentBuilder } from '@nestjs/swagger';
 
 export function getSwaggerDocumentationConfig() {
   return new DocumentBuilder()
-    .setTitle('Energy Web Zero API')
+    .setTitle('Energy Web Zero PL POC API')
     .setVersion('0.3')
     .addApiKey({ type: 'apiKey', name: 'X-API-KEY' }, 'api-key')
     .build();
