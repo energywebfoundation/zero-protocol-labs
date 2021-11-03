1ebb231128d4c646aeb98bd4a7f8e23a24d1dd23
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2021-11-03 17:43:11 +0100

    chore(backend): added certificate generation time timezone offsets

--- apps/backend/src/certificates/dto/certificate.dto.ts
+++ apps/backend/src/certificates/dto/certificate.dto.ts
@@ -1,4 +1,4 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class CertificateDto {
   @ApiProperty({ example: '973d48bb-15da-4eaf-8040-b6cb66e22023' })
@@ -22,8 +22,14 @@ export class CertificateDto {
   @ApiProperty({ example: new Date('2020-11-01T00:00:00.000Z') })
   generationStart: string;
 
+  @ApiPropertyOptional({ example: 180 })
+  generationStartTimezoneOffset: number;
+
   @ApiProperty({ example: new Date('2021-06-01T23:59:59.999Z') })
   generationEnd: string;
+
+  @ApiPropertyOptional({ example: 180 })
+  generationEndTimezoneOffset: number;
 
   @ApiProperty({ example: '0x65ca0692df73b3ff23126fd69e15d2f7de7a317def6016ebfdeedde1e24a7a8f' })
   txHash: string;
