c4d572619f6f1b3c65827eb906841d1be7937999
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2021-11-04 14:03:37 +0100

    chore(backend): validate Buyer payload

--- apps/backend/src/buyers/dto/create-buyer.dto.ts
+++ apps/backend/src/buyers/dto/create-buyer.dto.ts
@@ -1,9 +1,14 @@
-import { ApiProperty } from "@nestjs/swagger";
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsOptional, IsString, IsUUID } from 'class-validator';
 
 export class CreateBuyerDto {
-  @ApiProperty({ example: '00000000-0000-0000-0000-000000000000' })
+  @ApiPropertyOptional({ example: '00000000-0000-0000-0000-000000000000' })
+  @IsUUID()
+  @IsOptional()
   id: string;
 
-  @ApiProperty({ example: '-' })
+  @ApiPropertyOptional({ example: '-' })
+  @IsString()
+  @IsOptional()
   name: string;
 }
