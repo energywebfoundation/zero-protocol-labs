36b0595720fd4e83c93ec5053165d2c470c3709b
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2021-11-09 13:36:48 +0100

    fix(backend): PATCH /api/partners/filecoin/certificates/{id} crashing

--- apps/backend/src/certificates/dto/update-certificate.dto.ts
+++ apps/backend/src/certificates/dto/update-certificate.dto.ts
@@ -1,4 +1,8 @@
-import { PartialType } from '@nestjs/swagger';
+import { OmitType, PartialType } from '@nestjs/swagger';
+import { Exclude } from 'class-transformer';
 import { CreateCertificateDto } from './create-certificate.dto';
 
-export class UpdateCertificateDto extends PartialType(CreateCertificateDto) {}
+export class UpdateCertificateDto extends PartialType(OmitType(CreateCertificateDto, ['energy'])) {
+  @Exclude()
+  energy: string;
+}
