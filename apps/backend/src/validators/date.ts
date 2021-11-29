fc2713d6de0dcb4527e6cd27046522ab985607f6
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2021-11-29 17:43:57 +0100

    fix(backend): validate datetime strings to be parseable by Prisma client

--- apps/backend/src/validators/date.ts
+++ apps/backend/src/validators/date.ts
@@ -3,7 +3,7 @@ import { registerDecorator, ValidationArguments, ValidationOptions } from 'class
 /**
  * validates if a string is parseable by the Date object constructor
  */
-export function IsDateParseable(validationOptions?: ValidationOptions) {
+export function IsDatetimePrismaCompatible(validationOptions?: ValidationOptions) {
   return function(object: unknown, propertyName: string) {
     registerDecorator({
       name: 'IsDateParseable',
@@ -12,10 +12,10 @@ export function IsDateParseable(validationOptions?: ValidationOptions) {
       options: validationOptions,
       validator: {
         validate(value: string): boolean {
-          return (new Date(value).toString() !== 'Invalid Date');
+          return /\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z)/.test(value);
         },
         defaultMessage(args?: ValidationArguments): string {
-          return `${args.property} should be parseable by the Date object constructor`;
+          return `${args.property} should have format: 2021-01-01T00:00:00.000+08:00 or 2021-04-30T21:59:59.999Z`;
         }
       }
     });
